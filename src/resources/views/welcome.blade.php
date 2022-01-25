a71b1e2be330c1a9ba41afffc796a910d1a59d85
hnsa9 <hanisa.nine@gmail.com>
2022-01-25 14:18:35 +0800

    add register login scaffold

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+
+        <!-- Include Frontend Application (webpack mix) -->
+        <script defer src="/js/manifest.js"></script>
+        <script defer src="/js/vendor.js"></script>
+        <script defer src="/js/app.js"></script>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
